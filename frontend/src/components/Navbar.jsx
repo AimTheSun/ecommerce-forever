3bfeece07b93bbeb273e02a811c3851846c67a8e
AimTheSun <mirassol.pedro7@gmail.com>
2025-03-03 20:24:54 +0000

    feat: added login/sign UI and About US component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,12 +8,14 @@ import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false); // State to show/hide the navbar on small screens
 
-  const {setShowSearch, getCartCount} = useContext(ShopContext);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
   return (
     <div className="flex item-center justify-between py-5 font-medium">
-      
-      <Link to="/"> <img src={assets.logo} className="w-36" alt="" /></Link>
+      <Link to="/">
+        {" "}
+        <img src={assets.logo} className="w-36" alt="" />
+      </Link>
 
       <ul className="hidden sm:flex gap-5 text-sm text-gray-700 ">
         <NavLink to="/" className="flex flex-col items-center gap-1">
@@ -38,14 +40,22 @@ const Navbar = () => {
       </ul>
 
       <div className="flex items-center gap-6">
-        <img onClick={() => setShowSearch(true)}   src={assets.search_icon} className="w-5 cursor-pointer " alt="" />
-
+             
+          <img
+            onClick={() => setShowSearch(true)}
+            src={assets.search_icon}
+            className="w-5 cursor-pointer "
+            alt=""
+          />
+       
         <div className="group relative">
+        <Link to='/login'> 
           <img
             className="w-5 cursor-pointer"
             src={assets.profile_icon}
             alt=""
           />
+           </Link>
           <div className="hidden group-hover:block absolute top-4 right-0 w-36 bg-slate-100 text-gray-500 border shadow-lg">
             <div className="flex flex-col gap-2 py-3 px-5">
               <p className="cursor-pointer hover:text-black"> My Profile </p>
@@ -57,7 +67,7 @@ const Navbar = () => {
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-         {getCartCount()}
+            {getCartCount()}
           </p>
         </Link>
         <img
