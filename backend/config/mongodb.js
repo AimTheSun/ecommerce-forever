370d7a55b009aeeb8fa3e37cff07fd7b7d35924c
AimTheSun <mirassol.pedro7@gmail.com>
2025-03-07 02:08:05 +0000

    feat: vercel mongodb deployment

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,21 +1,11 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  try {
+
     mongoose.connection.on("connected", () => console.log("Connected to MongoDB"));
-    mongoose.connection.on("error", (err) => console.error(`MongoDB connection error: ${err}`));
-    mongoose.connection.on("disconnected", () => console.log("Disconnected from MongoDB"));
 
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(`${process.env.MONGODB_URI}/e-commerce`);
 
-    console.log("MongoDB connection established");
-  } catch (error) {
-    console.error(`Error connecting to MongoDB: ${error.message}`);
-    process.exit(1);
-  }
-};
+}
 
 export default connectDB;
